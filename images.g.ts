003f5bd57be05efcc6909ecb08f424b87144e6e2
AnnyHiHi <89023544+AnnyHiHi@users.noreply.github.com>
2022-02-11 11:52:58 +0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,59 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+....................
+......f.bbbb.f......
+.....ff111111ff.....
+.....b15583351b.....
+....b18ff11ff81b....
+....b8fdf11fdf8b....
+....b5ff1ff1ff3b....
+....b1811111181b....
+....b1183358511b....
+...fb1111111111bf...
+..ffb1111111111bff..
+.ff..1118572111..ff.
+.6f9.b11111111b.9f6.
+.9.99fb111111bf99.9.
+.6.69ffbbbbbbff96.6.
+.9..9ffff..ffff9..9.
+.99.6ffff..ffff6.99.
+.69.666669966666.96.
+..66.9999..9999.66..
+....................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
